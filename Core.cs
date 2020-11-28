fb75087cdc7b80dac2b74e61b03b8a453470bc49
Luis Guijarro Pérez <luisgp82@hotmail.es>
2020-11-28 21:40:08 +0100

    Creamos Clase de nucleo con metodos internos de control de Ids de objetos por color.

---
+++ Core.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+
+namespace dge
+{    
+    internal static class Core2D
+    {
+        internal static Dictionary<int, int> IDS;
+        internal static int GetID() // Otorga un ID a objeto que lo solicita
+        {
+            if (IDS == null)
+            {
+                IDS = new Dictionary<int, int>();
+            }
+            for (int i=0;i<IDS.Count;i++)
+            {
+                if (!IDS.ContainsKey(i))
+                {
+                    IDS.Add(i,i);
+                    return i;
+                }
+            }
+            int e = IDS.Count;
+            IDS.Add(e,e);
+            return e;
+        }
+
+        internal static bool ReleaseID(int id) // Liberia ID de objeto y devuelve si ha tenido exito.
+        {
+            if (IDS == null) { return false; }
+            if (IDS.ContainsKey(id))
+            {
+                IDS.Remove(id);
+                return true;
+            }
+            return false;
+        }
+
+        internal static byte[] DeIntAByte4(int num) //
+		{
+			byte[] ret=new byte[4];
+			
+			for(int t=1;t<3;t++)
+			{
+				ret[t]=(byte)(num%256);
+				num=(num-num%256)/256;
+			}
+			ret[3] = 255;
+			return ret;
+		}
+
+		internal static int DeByte4AInt(byte[] num)
+		{
+			return num[1]+num[2]*256; //+num[2]*256*256; //+num[3]*256*256*256
+		}
+    }
+}
