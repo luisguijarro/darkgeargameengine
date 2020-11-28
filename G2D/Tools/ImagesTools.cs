ab3da8d9bacaf9cfc0f0ef79d0be174518148dab
Luis Guijarro Pérez <luisgp82@hotmail.es>
2020-11-28 21:30:03 +0100

    Creamos Herramientas de carga de imagenes.

---
+++ G2D/Tools/ImagesTools.cs
@@ -0,0 +1,105 @@
+using System;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Collections.Generic;
+
+using dge.G2D;
+
+using dgtk.OpenGL;
+
+namespace dge.G2D
+{
+    public static partial class Tools
+    {
+        internal static Dictionary<string, TextureBufferObject> d_imageshash = new Dictionary<string, TextureBufferObject>();
+
+        public static TextureBufferObject LoadImage(string filepath)
+        {
+            string s_hash = FileTools.MD5FromFile(filepath);
+
+            if (d_imageshash.ContainsKey(s_hash))
+            {
+                return d_imageshash[s_hash];
+            }
+            d_imageshash.Add(s_hash, p_LoadImage(filepath, s_hash));
+            return d_imageshash[s_hash];
+        }
+
+        public static unsafe TextureBufferObject LoadImageFromIntPTr(string name, string hash, int Width, int Height, IntPtr Scan0)
+		{
+            if (d_imageshash.ContainsKey(hash))
+            {
+                return d_imageshash[hash];
+            }
+            d_imageshash.Add(hash, p_LoadImageFromIntPTr(name, Width, Height, Scan0));
+            return d_imageshash[hash];
+        }
+
+        private static TextureBufferObject p_LoadImage(string filepath, string s_hash)
+        {
+            TextureBufferObject tbo_ret = new TextureBufferObject();
+            if (File.Exists(filepath))
+			{
+                Bitmap bp = new Bitmap(filepath);
+                BitmapData bd = bp.LockBits(new Rectangle(0, 0, bp.Size.Width, bp.Size.Height), ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+							
+				tbo_ret = p_LoadImageFromIntPTr(filepath, bd.Width, bd.Height, bd.Scan0);
+							
+				bp.UnlockBits(bd);
+				bp.Dispose();
+				
+				return tbo_ret;
+            }
+            #if DEBUG
+            Console.WriteLine("Error: File "+filepath+"not exist.");
+            #endif
+            return tbo_ret;
+        }
+
+        private static unsafe TextureBufferObject p_LoadImageFromIntPTr(string name, int Width, int Height, IntPtr Scan0)
+		{
+            bool current = dgtk.OpenGL.OGL_SharedContext.MakeCurrent();
+            TextureBufferObject tbo_ret = new TextureBufferObject(); // Valor a devolver.
+
+            //dgtk.OpenGL.OGL_SharedContext.MakeCurrent(); // Falta el SharedContext
+
+            UInt32 idret = 0; //stackalloc uint[1];
+            GL.glEnable(EnableCap.GL_TEXTURE_2D);
+
+            idret = GL.glGenTexture();
+            GL.glBindTexture(TextureTarget.GL_TEXTURE_2D, idret);
+
+            GL.glTexParameteri(TextureTarget.GL_TEXTURE_2D, TextureParameterName.GL_TEXTURE_MIN_FILTER, (int)TextureMinFilter.GL_LINEAR);
+            GL.glTexParameteri(TextureTarget.GL_TEXTURE_2D, TextureParameterName.GL_TEXTURE_MAG_FILTER, (int)TextureMinFilter.GL_LINEAR);
+            GL.glTexParameteri(TextureTarget.GL_TEXTURE_2D, TextureParameterName.GL_TEXTURE_WRAP_S, (int)TextureWrapMode.GL_REPEAT);
+            GL.glTexParameteri(TextureTarget.GL_TEXTURE_2D, TextureParameterName.GL_TEXTURE_WRAP_T, (int)TextureWrapMode.GL_REPEAT);
+
+            GL.glTexEnvi(TextureEnvTarget.GL_TEXTURE_ENV, TextureEnvParameter.GL_TEXTURE_ENV_MODE, (int)TextureEnvMode.GL_DECAL);
+
+            GL.glTexImage2D(TextureTarget.GL_TEXTURE_2D, 0, InternalFormat.GL_RGBA, Width, Height, 0, dgtk.OpenGL.PixelFormat.GL_BGRA, PixelType.GL_UNSIGNED_BYTE, Scan0);
+
+            GL.glBindTexture(TextureTarget.GL_TEXTURE_2D, 0);
+
+            GL.glFlush();
+
+            //dgtk.OpenGL.OGL_SharedContext.MakeCurrent();
+
+            return tbo_ret = new TextureBufferObject(name, (uint)Width, (uint)Height, idret, name);
+        }
+        
+		public static bool SaveImage(TextureBufferObject tbo, string filepath)
+		{
+			
+			return false;
+			
+		}
+		
+		public static bool SaveScreenShot( string filepath)
+		{
+			
+			return false;
+			
+		}
+    }
+}
