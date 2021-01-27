72ca625960098336e521eb96920a7ec0129ccc76
Luis Guijarro Pérez <luisgp82@hotmail.es>
2021-01-27 01:48:54 +0100

    Empleamos metodos de apertura de archivos de sonido dependiendo del sistema operativo. En .Net5 no hay dll map como en Mono.

--- SoundSystem/SndSystem.cs
+++ SoundSystem/SndSystem.cs
@@ -22,11 +22,11 @@ namespace dge
                 {
                     SF_INFO sf_info = new SF_INFO();
                     sf_info.format = 0;
-                    IntPtr ptr_snd = Imports.sf_open(path, OpenMode.SFM_READ, ref sf_info);
+                    IntPtr ptr_snd = dge.Core.GetOS() == dge.Core.OperatingSystem.Windows ? ImportsW.sf_open(path, OpenMode.SFM_READ, ref sf_info) : ImportsL.sf_open(path, OpenMode.SFM_READ, ref sf_info);
                         #if DEBUG
                         if (ptr_snd == IntPtr.Zero) // Si se ha dado algun error...
                         {
-                            Console.WriteLine(Imports.sf_error(ptr_snd).ToString()); // Hay que saber cual.
+                            Console.WriteLine(dge.Core.GetOS() == dge.Core.OperatingSystem.Windows ? ImportsW.sf_error(ptr_snd).ToString() : ImportsL.sf_error(ptr_snd).ToString()); // Hay que saber cual.
                             return null;
                         }
                         #endif
@@ -34,14 +34,21 @@ namespace dge
                     List<short> l_data = new List<short>();
                     short[] data = new short[4096];
                     long datareaded = 0;
-                    while ((datareaded = Imports.sf_read_short(ptr_snd, ref data, 4096)) != 0)
+                    while ((datareaded = (dge.Core.GetOS() == dge.Core.OperatingSystem.Windows ? ImportsW.sf_read_short(ptr_snd, ref data, 4096) : ImportsL.sf_read_short(ptr_snd, ref data, 4096))) != 0)
                     {
                         short[] s_datatemp = new short[datareaded];
                         Array.Copy(data, s_datatemp, datareaded);
                         l_data.AddRange(s_datatemp);
                     }
                     data = l_data.ToArray();
-                    Imports.sf_close(ptr_snd);
+                    if (dge.Core.GetOS() == dge.Core.OperatingSystem.Windows)
+                    {
+                        ImportsW.sf_close(ptr_snd);
+                    }
+                    else
+                    {
+                        ImportsL.sf_close(ptr_snd);
+                    }
 
                     //s_ret.FileName = path;
                     // AL_FORMAT alf = sf_info.channels > 1? AL_FORMAT.AL_FORMAT_STEREO16 : AL_FORMAT.AL_FORMAT_MONO16;
