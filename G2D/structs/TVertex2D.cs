0ddfa33b76eb4580b6d1653d7968c5864266ab3e
Luis Guijarro Pérez <luisgp82@hotmail.es>
2021-02-13 02:08:53 +0100

    Añadimos atributo ID al Vertice.

--- G2D/structs/TVertex2D.cs
+++ G2D/structs/TVertex2D.cs
@@ -7,10 +7,12 @@ namespace dge.G2D
     [StructLayout(LayoutKind.Sequential)]
     public struct TVertex2D
     {
+        public int ID;
         public Vector2 Position;
         public Vector2 TexCoord;
-        public TVertex2D (Vector2 position, Vector2 texcoord)
+        public TVertex2D (int id, Vector2 position, Vector2 texcoord)
         {
+            this.ID = id;
             this.Position = position;
             this.TexCoord = texcoord;
         }
