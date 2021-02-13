4129a7cf8dc38bf918950fd16c468435ac2a80f4
Luis Guijarro Pérez <luisgp82@hotmail.es>
2021-02-13 02:14:50 +0100

    Creamos estructura de fuente de letra para Dark Gear Engine.

---
+++ G2D/structs/dgFont.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+
+namespace dge.G2D
+{    
+    public struct dgFont
+    {
+        internal string s_Name;
+        internal float f_MaxFontSize;
+        internal float f_borderWidth;
+        internal float f_spaceWidth;
+		internal TextureBufferObject TBO_Scan0; // Textura con el relleno de la fuente
+		internal TextureBufferObject TBO_Scan1; // Textura con el borde de la fuente
+        internal Dictionary<char, dgCharacter> d_characters;
+
+        internal dgFont(string name, float max_font_size, float borderWidth, float spaceWidth, char[] charkeys, dgCharacter[] charvalues, TextureBufferObject scan0, TextureBufferObject scan1)
+        {
+            this.s_Name = name;
+            this.f_MaxFontSize = max_font_size;
+            this.f_borderWidth = borderWidth;
+            this.f_spaceWidth = spaceWidth;
+            this.TBO_Scan0 = scan0;
+            this.TBO_Scan1 = scan1;
+            if (charkeys.Length != charvalues.Length)
+            {
+                throw new Exception("glFont Constructor Error: The numbar of keys and values is not the same.");
+            }
+            this.d_characters = new Dictionary<char, dgCharacter>();
+            for (int i=0;i<charkeys.Length;i++)
+            {
+                this.d_characters.Add(charkeys[i], charvalues[i]);
+            }
+        }
+
+        #region PROPERTIES
+
+        public string Name
+        {
+            get {return this.s_Name;}
+        }
+
+        public float MaxFontSize
+        {
+            get { return this.f_MaxFontSize; }
+        }
+
+        public float BorderWidth
+        {
+            get { return this.f_borderWidth; }
+        }
+
+        #endregion
+    }
+}
