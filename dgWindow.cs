2624c1422eba73533acd102bea41c7ff02369cec
Luis Guijarro Pérez <luisgp82@hotmail.es>
2021-02-25 00:56:33 +0100

    Declaramos GUI y Dijujante de GUI.

--- dgWindow.cs
+++ dgWindow.cs
@@ -6,6 +6,7 @@ namespace dge
     public partial class dgWindow : dgtk.dgtk_Window
     {
         private G2D.Drawer drawer2D;
+        private G2D.GuiDrawer GuiDrawer2D;
         private G2D.Writer writer2D;
         private GUI.GraphicsUserInterface gui;
         private SndSystem sndSystem;
@@ -14,6 +15,8 @@ namespace dge
             sndSystem = new SndSystem(base.OpenALContext);
             this.MakeCurrent();
             this.drawer2D = new G2D.Drawer();
+            this.GuiDrawer2D = new G2D.GuiDrawer();
+            this.GuiDrawer2D.DefinePerspectiveMatrix(0,0,this.Width, this.Height);
             this.writer2D = new G2D.Writer(this);
             this.UnMakeCurrent(); //No debería ser necesario.
             dge.G2D.IDsDrawer.Init_IDs_Drawer(); // Iniciamos Código de visualizado de Ids.
@@ -25,7 +28,7 @@ namespace dge
             base.OnRenderFrame(sender, e);
             if (this.gui != null)
             {
-                gui.Draw(this.drawer2D);
+                gui.Draw(this.GuiDrawer2D);
             }
         }
 
