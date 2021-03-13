1266099c1a00be703ebd11539f8186e42253221d
Luis Guijarro Pérez <luisgp82@hotmail.es>
2021-03-14 00:19:24 +0100

    Creamos enumeradores para propiedades de controles de GUI.

---
+++ GUI/Enums.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace dge.GUI
+{ 
+    public enum Orientation
+    {
+        Horizontal = 0, Vertical = 1
+    }
+
+    public enum SliderShape
+    {
+        Arrow = 0, Square = 1
+    }
+
+    public enum SliderOrientation
+    {
+        UpRigth = 0, BottomLeft = 1
+    }
+
+    public enum TextAlign
+    {
+        Left = 0, Center = 1, Right = 2
+    }
+}
