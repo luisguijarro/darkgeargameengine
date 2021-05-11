34d369e62c7630eda05e67189a0c39fbc49a5b25
Luis Guijarro Pérez <luisgp82@hotmail.es>
2021-05-11 21:16:13 +0200

    Añadimos structura de control de entrada y salida de fichero virtual.

--- SoundSystem/LibSndFile/Structs.cs
+++ SoundSystem/LibSndFile/Structs.cs
@@ -17,4 +17,21 @@ namespace dge.SoundSystem
             get { return format != 0 && channels > 0 && samplerate > 0; }
         }
 	}
+
+	[StructLayout(LayoutKind.Sequential)]
+	internal struct SF_VIRTUAL_IO
+    {    
+		internal sf_vio_get_filelen get_filelen ;
+        internal sf_vio_seek seek ;
+        internal sf_vio_read read ;
+        internal sf_vio_write write ;
+        internal sf_vio_tell tell ;
+    }
+
+	delegate long sf_vio_get_filelen (IntPtr user_data) ;
+	delegate long  sf_vio_seek (long offset, int whence, IntPtr user_data) ;
+	delegate long  sf_vio_read (IntPtr ptr, long count, IntPtr user_data) ;
+	delegate long  sf_vio_write (IntPtr ptr, long count, IntPtr user_data) ;
+	delegate long  sf_vio_tell (IntPtr user_data) ;
+
 }
