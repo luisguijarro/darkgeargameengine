56cfec74ff82da24405c693d24feed04c7e52166
Luis Guijarro Pérez <luisgp82@hotmail.es>
2021-05-11 21:08:51 +0200

    Creacion de metodos y atributos de parentesco y dibujado. Creamos atributo Name.

--- Scene/Scene.cs
+++ Scene/Scene.cs
@@ -6,13 +6,27 @@ namespace dge
 
     public class Scene : Scenenode
     {
-        public Scene() : base()
+        protected string s_name;
+        protected dgWindow parentWin;
+        public Scene(string name) : base()
+        {
+            this.s_name = name;
+        }
+        internal void InternalDraw()
+        {
+            this.Draw();
+        }
+        protected virtual void Draw() // Dibuja la escena.
         {
 
         }
-        internal void Draw() // Dibuja la escena.
+        internal virtual void SetParentWindow(dgWindow win)
         {
-
+            parentWin = win;
+        }
+        public string Name
+        {
+            get { return this.s_name; }
         }
     }
 
