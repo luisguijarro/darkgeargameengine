8900c489596a82606f7958601bca0190f0c781c7
Luis Guijarro Pérez <luisgp82@hotmail.es>
2021-09-06 02:39:11 +0200

    Definimos propiedad de alineamiento igual que en TextBox.

--- GUI/Label.cs
+++ GUI/Label.cs
@@ -14,6 +14,7 @@ namespace dge.GUI
         private Color4 c4_bordercolor;
         private float tx_x, tx_y; // Coordenadas de texto
         private bool FirsDraw;
+        private TextAlign ta_textAlign;
         public Label() : this(22,22, "Text")
         {
             
@@ -23,14 +24,17 @@ namespace dge.GUI
         {
             this.FirsDraw = true;
             this.s_text = text;
-            this.FontSize = 16;
+            this.FontSize = 14;
+            this.ta_textAlign = TextAlign.Left;
             this.font = GuiTheme.DefaultGuiTheme.DefaultFont;
-            this.c4_forecolor = dgtk.Graphics.Color4.Black;
-            this.c4_bordercolor = dgtk.Graphics.Color4.Black;
+            this.c4_forecolor = GuiTheme.DefaultGuiTheme.DefaultTextColor;
+            this.c4_bordercolor = GuiTheme.DefaultGuiTheme.DefaultTextBorderColor;
         }
 
         protected internal override void UpdateTheme()
         {
+            this.c4_forecolor = this.gui.GuiTheme.DefaultTextColor;
+            this.c4_bordercolor = this.gui.GuiTheme.DefaultTextBorderColor;
             // Si la fuente establecida es la del tema por defecto se cambia, sino, se deja la establecida por el usuario.
             if (this.font.Name == GuiTheme.DefaultGuiTheme.DefaultFont.Name)
             {
@@ -52,14 +56,38 @@ namespace dge.GUI
             }
         }
 
-        private void UpdateTextCoords(float fsize)
+        private void UpdateTextCoords()
         {
+            /*
             if (this.gui != null)
             {
                 if (this.gui.Writer != null)
                 {
-                    this.tx_x = (this.i_width/2f) - (dge.G2D.Writer.MeasureString(this.font, this.s_text, fsize)[0]/2f);
-                    this.tx_y = (this.i_height/2.1f) - (fsize/1.2f);
+                    this.tx_x = 0; //(this.i_width/2f) - (dge.G2D.Writer.MeasureString(this.font, this.s_text, this.f_fs)[0]/2f);
+                    this.tx_y = (this.i_height/2.1f) - (this.f_fs/1.2f);
+                }
+            }
+            */
+            if (this.gui != null)
+            {
+                if (this.gui.Writer != null)
+                {
+                    float txtsize = G2D.Writer.MeasureString(this.font, this.s_text, this.f_fs)[0];
+
+                    switch(this.ta_textAlign)
+                    {
+                        case TextAlign.Left:
+                            this.tx_x = this.MarginsFromTheEdge[0] * 2;
+                            break;
+                        case TextAlign.Center:
+                            this.tx_x = (this.i_width/2f) - (txtsize/2f);
+                            break;
+                        case TextAlign.Right:
+                            this.tx_x = this.i_width-(this.MarginsFromTheEdge[2]*2) - txtsize;
+                            break;
+                    }
+
+                    this.tx_y = (this.i_height/2.1f) - (this.f_fs/1.2f);
                 }
             }
         }
@@ -67,7 +95,7 @@ namespace dge.GUI
 
         protected override void pDraw()
         {   
-            if (this.FirsDraw) { this.UpdateTextCoords(this.f_fs); this.FirsDraw  =false; };
+            if (this.FirsDraw) { this.UpdateTextCoords(); this.FirsDraw  =false; };
             if (this.gui != null)
             {
                 this.DrawIn(this.i_x+this.MarginsFromTheEdge[0],this.i_y+this.MarginsFromTheEdge[1],this.i_width-(this.MarginsFromTheEdge[0]+this.MarginsFromTheEdge[2]), this.i_height-(this.MarginsFromTheEdge[1]+this.MarginsFromTheEdge[3]), DrawText);
@@ -86,7 +114,7 @@ namespace dge.GUI
         
         public dgtk.Graphics.Color4 TextColor
         {
-            set { this.c4_forecolor = value;  this.UpdateTextCoords(this.f_fs);}
+            set { this.c4_forecolor = value;  this.UpdateTextCoords();}
             get { return this.c4_forecolor; }
         }
 
@@ -104,21 +132,28 @@ namespace dge.GUI
 
         public float FontSize 
         {
-            set { this.f_fs = value; this.UpdateTextCoords(this.f_fs); }
+            set { this.f_fs = value; this.UpdateTextCoords(); }
             get { return this.f_fs; }
         }
 
         public dgFont Font
         {
-            set { this.font = value; this.UpdateTextCoords(this.f_fs); }
+            set { this.font = value; this.UpdateTextCoords(); }
             get { return this.font; }
         }
 
         public string Text
         {
-            set { this.s_text = value; this.UpdateTextCoords(this.f_fs); }
+            set { this.s_text = value; this.UpdateTextCoords(); }
             get { return this.s_text; }
         }
+
+        public TextAlign TextAlign
+        {
+            set { this.ta_textAlign = value; this.UpdateTextCoords(); }
+            get { return this.ta_textAlign; }
+        }
+
 
         #endregion
 
