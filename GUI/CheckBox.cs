ebaf4e72452893a675b92111435727ce490f4b23
Luis Guijarro Pérez <luisgp82@hotmail.es>
2021-09-22 23:27:54 +0200

    Añadimos modo visual de boton, asi como posibilidad de establecer imagen.

--- GUI/CheckBox.cs
+++ GUI/CheckBox.cs
@@ -8,7 +8,9 @@ namespace dge.GUI
 {
     public class CheckBox : BaseObjects.Control
     {
-        private bool b_Checked;
+        protected bool b_Checked;
+        protected bool b_ButtonMode;
+        private TextureBufferObject tbo_image;
         public event EventHandler<CheckedStateChanged> CheckedStateChanged;
         public CheckBox() : base(22, 22)
         {
@@ -19,6 +21,8 @@ namespace dge.GUI
             this.tcFrameOffset = GuiTheme.DefaultGuiTheme.CheckBox_FrameOffset;
 
             this.CheckedStateChanged += delegate{};
+
+            this.tbo_image = TextureBufferObject.Null;
         }
 
         protected internal override void UpdateTheme()
@@ -26,6 +30,24 @@ namespace dge.GUI
             this.MarginsFromTheEdge = this.gui.gt_ActualGuiTheme.CheckBox_MarginsFromTheEdge;            
             this.Texcoords = this.gui.gt_ActualGuiTheme.CheckBox_Texcoords;
             this.tcFrameOffset = this.gui.gt_ActualGuiTheme.CheckBox_FrameOffset;
+            this.SetGraphicMode(this.b_ButtonMode);
+        }
+
+        protected override void OnGuiUpdate()
+        {
+            base.OnGuiUpdate();
+            if (this.b_ButtonMode)
+            {
+                this.MarginsFromTheEdge = this.gui.GuiTheme.Button_MarginsFromTheEdge;            
+                this.Texcoords = this.gui.GuiTheme.Button_Texcoords;
+                this.tcFrameOffset = this.gui.GuiTheme.Button_FrameOffset;
+            }
+            else
+            {
+                this.MarginsFromTheEdge = this.gui.GuiTheme.CheckBox_MarginsFromTheEdge;            
+                this.Texcoords = this.gui.GuiTheme.CheckBox_Texcoords;
+                this.tcFrameOffset = this.gui.GuiTheme.CheckBox_FrameOffset;
+            }
         }
 
         protected override void OnMDown(object sender, MouseButtonEventArgs e)
@@ -44,9 +66,65 @@ namespace dge.GUI
             {
                 //base.Draw();
                 this.gui.gd_GuiDrawer.DrawGL(this.gui.GuiTheme.ThemeTBO.ID, Color4.White, this.i_x, this.i_y, this.i_width, this.i_height, 0, this.MarginsFromTheEdge, Texcoords, this.b_Checked ? this.tcFrameOffset : new float[]{0,0}, 0);
+                if (this.b_ButtonMode)
+                {
+                    if (tbo_image.ID>0)
+                    {
+                        this.gui.Drawer.Draw(this.tbo_image.ui_ID, this.X+this.MarginLeft, this.Y+this.MarginTop, this.i_width-(this.MarginLeft+this.MarginRight), this.i_height-(this.MarginTop+this.MarginBottom), 0f, 0f, 0f, 1f, 1f);
+                    }
+                }
             }
         }
 
+        public void SetGraphicMode(bool Is_Like_Button)
+        {
+            if (this.gui != null)
+            {
+                if (Is_Like_Button)
+                {
+                    this.MarginsFromTheEdge = this.gui.GuiTheme.Button_MarginsFromTheEdge;            
+                    this.Texcoords = this.gui.GuiTheme.Button_Texcoords;
+                    this.tcFrameOffset = this.gui.GuiTheme.Button_FrameOffset;
+                }
+                else
+                {
+                    this.MarginsFromTheEdge = this.gui.GuiTheme.CheckBox_MarginsFromTheEdge;            
+                    this.Texcoords = this.gui.GuiTheme.CheckBox_Texcoords;
+                    this.tcFrameOffset = this.gui.GuiTheme.CheckBox_FrameOffset;
+                }
+            }
+            else
+            {
+                if (Is_Like_Button)
+                {
+                    this.MarginsFromTheEdge = GuiTheme.DefaultGuiTheme.Button_MarginsFromTheEdge;            
+                    this.Texcoords = GuiTheme.DefaultGuiTheme.Button_Texcoords;
+                    this.tcFrameOffset = GuiTheme.DefaultGuiTheme.Button_FrameOffset;
+                }
+                else
+                {
+                    this.MarginsFromTheEdge = GuiTheme.DefaultGuiTheme.CheckBox_MarginsFromTheEdge;            
+                    this.Texcoords = GuiTheme.DefaultGuiTheme.CheckBox_Texcoords;
+                    this.tcFrameOffset = GuiTheme.DefaultGuiTheme.CheckBox_FrameOffset;
+                }
+            }
+            this.b_ButtonMode = Is_Like_Button;
+        }
+
+        public bool GraphicalButtonMode
+        {
+            set
+            {
+                this.SetGraphicMode(value);
+            }
+            get { return this.b_ButtonMode; }
+        }
+
+        public TextureBufferObject Image
+        {
+            set { this.tbo_image = value; }
+            get { return this.tbo_image; }
+        }
         public bool Checked
         {
             set { this.b_Checked = value; }
