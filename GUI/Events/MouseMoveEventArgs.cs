82c0e3ded2ae31be71edf72202d155da2bb30829
Luis Guijarro Pérez <luisgp82@hotmail.es>
2021-10-01 11:00:10 +0200

    Añadimos parametro de ID de objeto para el que se lanza el evento. Añadimos parametros de ultima posicion del raton.

--- GUI/Events/MouseMoveEventArgs.cs
+++ GUI/Events/MouseMoveEventArgs.cs
@@ -4,16 +4,22 @@ namespace dge.GUI
 {
     public class MouseMoveEventArgs : EventArgs
     {
+        private uint ui_id;
         private int i_posx;
         private int i_posy;
         private int i_spX;
         private int i_spY;
-        public MouseMoveEventArgs(int posX, int posY, int posXScreen, int posYScreen)
+        private int i_lastPosX;
+        private int i_lastPosY;
+        public MouseMoveEventArgs(int posX, int posY, int lastposx, int lastposy, int posXScreen, int posYScreen, uint id)
         {
             this.i_posx = posX;
             this.i_posy = posY;
+            this.i_lastPosX = lastposx;
+            this.i_lastPosY = lastposy;
             this.i_spX = posXScreen;
             this.i_spY = posYScreen;
+            this.ui_id = id;
         }
         public int X 
         {
@@ -23,6 +29,15 @@ namespace dge.GUI
         {
             get { return this.i_posy; }
         }
+        public int LastPosX 
+        {
+            get { return this.i_lastPosX; }
+        }
+        public int LastPosY 
+        {
+            get { return this.i_lastPosY;}
+        }
+
         public int X_inScreen
         {
             get { return this.i_spX; }
@@ -30,6 +45,10 @@ namespace dge.GUI
         public int Y_inScreen
         {
             get { return this.i_spY; }
+        }
+        public uint ID 
+        {
+            get { return this.ui_id; }
         }
     }
 }
