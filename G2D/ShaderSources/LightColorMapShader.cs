654b21dc3f86db9031feed84336de9f9e427dd2d
Luis Guijarro Pérez <luisgp82@hotmail.es>
2021-11-08 01:03:09 +0100

    Terminamos de definir el FrameShader con pintado de blanco y aplicacion de gradiente a negro y a color.

--- G2D/ShaderSources/LightColorMapShader.cs
+++ G2D/ShaderSources/LightColorMapShader.cs
@@ -6,11 +6,9 @@ namespace dge.G2D
         
         layout(location = 0) in int vId;
         layout(location = 1) in vec2 vPos;
-
-        out vec3 VColor;
-        out vec3 SelectedColor;
         
         uniform vec3 InColor;
+        out vec3 SelectedColor;
         
         uniform vec2 v_size;
         uniform mat4 trasform;
@@ -25,23 +23,50 @@ namespace dge.G2D
         }
         ";
 
-        internal static string ColorLightMapfs = @"#version 330 core
-        
-        in vec3 VColor;
-        in vec3 SelectedColor;
-        out vec4 FragColor;
+        internal static string ColorLightMapfs = @"#version 400 core
 
-        void main()
-        {
-            float position = gl_PointCoord.y;
+in vec3 SelectedColor; // Color Inicial con el que trabajar.
+out vec4 FragColor; //Color de Salida del Shader.
+uniform float p_size; //Tamaño de cada lado del Punto.
+uniform vec2 v_size; //Tamaño a Visualizar.
+
+void main()
+{
+    float posXgl = gl_PointCoord.x/(1.0/p_size); //posicion en pixeles
+	float posYgl = gl_PointCoord.y/(1.0/p_size); //posicion en pixeles
+    if (posXgl <= 3) { posXgl = 0;}
+    if (posYgl <= 2) { posYgl = 0;}
+    if (posXgl >= v_size.x-2) { posXgl = v_size.x;}
+    if (posYgl >= v_size.y-2) { posYgl = v_size.y;}
+
+    float X_RelPosOnSize = (1/(v_size.x)) * posXgl; // Por1 de la posicion de pixeles en espacio pintable 
+	float Y_RelPosOnSize = (1/(v_size.y)) * posYgl; // Por1 de la posicion de pixeles en espacio pintable 
+
+    if (X_RelPosOnSize <= 1.0)
+	{
+		if (Y_RelPosOnSize <= 1.0)
+		{
+			//Pintamos
+            
             vec4 blanco = vec4(1,1,1,1);
-            vec4 negro = vec4(0,0,0, gl_PointCoord.y);
-
-            vec4 FinalColor = mix (vec4(SelectedColor.xyz, 1), blanco, 1-gl_PointCoord.x);
-            FinalColor = mix(FinalColor, negro, gl_PointCoord.y);
-           
-            FragColor = vec4(FinalColor.xyz, 1);
+            vec4 negro = vec4(0,0,0, Y_RelPosOnSize);
+            vec4 FinalColor = mix (vec4(SelectedColor.xyz, 1.0), blanco, 1.0-X_RelPosOnSize);
+            FinalColor = mix(FinalColor, negro, Y_RelPosOnSize);
+            
+            FragColor = vec4(FinalColor.xyz, 1.0);
         }
+        else
+        {
+            FragColor = vec4(1, 0.5, 1, 1);
+        }
+    }
+    else
+    {
+        FragColor = vec4(1, 0.5, 1, 1);
+    }
+    
+    // No pintamos
+}
         ";
     }
 }
