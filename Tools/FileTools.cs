dbd42f45e0127184a4f0b86345f52751207fc769
Luig Guijarro Pérez <luisgp82@hotmail.es>
2022-01-10 19:40:48 +0100

    Añadimos nuevos metodos para gestion de HAS por MD5.

--- Tools/FileTools.cs
+++ Tools/FileTools.cs
@@ -12,26 +12,17 @@ namespace dge.Tools
 	{
 		public static string MD5FromFile(string fileName)
 		{
-			//DateTime initdt = DateTime.Now;
 			FileStream file = new FileStream(fileName, FileMode.Open);
-			/*
-			MD5 md5 = new MD5CryptoServiceProvider();
-			byte[] retVal = md5.ComputeHash(file);
-			file.Close();
-			file.Dispose();
-			
-			StringBuilder sb = new StringBuilder();
-			for (int i=0; i< retVal.Length;i++)
-			{
-				sb.Append(retVal[i]);
-			}
-			//Console.WriteLine("Calcular MD5: "+(DateTime.Now-initdt).TotalMilliseconds+" milisegundos");
-			return sb.ToString();
-			*/
-			return MD5FromFile(file);
+			return MD5FromStream(file);
 		}
 
-		public static string MD5FromFile(Stream filestream)
+		public static string MD5FromBytes(byte[] bytes)
+		{
+			Stream file = new MemoryStream(bytes);
+			return MD5FromStream(file);
+		}
+
+		public static string MD5FromStream(Stream filestream)
 		{
 			//DateTime initdt = DateTime.Now;
 			//FileStream file = new FileStream(fileName, FileMode.Open);
