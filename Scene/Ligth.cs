1c46af2c6cac932b7fbc0f9941ee31e5e25aba1b
Luig Guijarro Pérez <luisgp82@hotmail.es>
2022-01-21 00:17:51 +0100

    Iniciamos la clase Light para el sistema de luces.

---
+++ Scene/Ligth.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+namespace dge
+{
+    public class Ligth
+    {
+        public Ligth(float x, float y) : this(x, y, 0) {}
+        public Ligth(float x, float y, float z)
+        {
+
+        }
+    }
+}
