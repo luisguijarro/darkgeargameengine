4b5e827e25e27276326e824e14031d54dba34053
Luig Guijarro Pérez <luisgp82@hotmail.es>
2022-02-02 13:26:57 +0100

    Añadimos eventos de actualizacion por UpdateFrame.

--- Scene/SceneNode.cs
+++ Scene/SceneNode.cs
@@ -7,11 +7,34 @@ namespace dge
     {
         protected string s_name;
         protected Dictionary<int,I_SceneNode> Nodes;
+        public event EventHandler<dgtk.dgtk_OnUpdateEventArgs> UpdateEvent;
         public SceneNode(string name)
         {
             this.s_name = name;
             this.Nodes = new Dictionary<int, I_SceneNode>();
+            this.UpdateEvent += delegate {};
         }
+
+        internal void InternalUpdate()
+        {
+            this.Update();
+            this.UpdateEvent(this, new dgtk.dgtk_OnUpdateEventArgs());
+        }
+
+        protected virtual void Update()
+        {
+
+        }
+        internal virtual void DrawIDs()
+        {
+            foreach (I_SceneNode node in this.Nodes.Values)
+            {
+                node.DrawIDs();
+            }
+        }
+
+
+
         public string Name
         {
             set { this.s_name = value; }
